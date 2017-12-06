a90f1a066be26ece477d1b956863af01d21678b2
hannah <wxh9301@hotmail.com>
2017-12-06 17:01:21 -0500

    path
    path change

--- ir_project_java/src/Classes/Path.java
+++ ir_project_java/src/Classes/Path.java
@@ -1,22 +1,16 @@
 package Classes;
 
 public class Path {
-	public static String StopwordDir="/Users/joel/Desktop/IRPROJ//stopword.txt";//address of stopwords.txt
-	public static String CleanData="/Users/joel/Desktop/IRPROJ//result.txt";//
-	public static String DataText="/Users/joel/Desktop/IRPROJ//text.txt";//
-	
-	
-	public static String DataRECIPEDir="/Users/joel/Downloads/epicurious-recipes-with-rating-and-nutrition//full_format_recipes.json";//address of docset.trectext
-	public static String IndexRECIPEDir="/Users/joel/Desktop/IRPROJ//indexRecipe//";//address of generated Text index file
+	public static String StopwordDir="/Users/hannah/Desktop/IRPROJ//stopword.txt";//address of stopwords.txt
+	public static String CleanData="/Users/hannah/Desktop/IRPROJ//result.txt";//
+	public static String DataText="/Users/hannah/Desktop/IRPROJ//text.txt";//
+
+
+	public static String DataRECIPEDir="/Users/hannah/Downloads/epicurious-recipes-with-rating-and-nutrition//full_format_recipes.json";//address of docset.trectext
+	public static String IndexRECIPEDir="/Users/hannah/Desktop/IRPROJ//indexRecipe//";//address of generated Text index file
 
 	public static String TopicDir="data//topics.txt";//address of topics.txt
-	
-	public static String biology="category//biology.txt";//address of topics.txt
-	public static String music="category//music.txt";//address of topics.txt
-	public static String politics="category//politics.txt";//address of topics.txt
-	public static String psychology="category//psychology.txt";//address of topics.txt
-	
-	public static String category="category//";//address of topics.txt
+
 
 
 }
